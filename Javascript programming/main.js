bebfec038844016d2bd1923dd8afe7a5c3e0ea75
deepakkumarshinde3 <deepakkumar.shinde3@gmail.com>
2023-05-20 20:36:34 +0530

    Javascript Basic

---
+++ Javascript programming/main.js
@@ -0,0 +1,9 @@
+// Type of Data in javascript
+let sName = "Deepakkumar"; // string
+let rollNo = 1234; // number
+let isPresent = false; // boolean (true/false)
+let marks = 50.5; // number
+let mobile; // undefined
+let parentNumber = null; // object (null , array , jsObject)
+
+// String , Number ,Boolean ,Object ,Undefined
---
+++ Javascript programming/variables.js
@@ -0,0 +1,35 @@
+// in-built function, object , classes
+// Native Function , Object , Classes
+// alert(1234); this will not run
+/*
+        ldsjf
+        dsfklkdfg
+      */
+//alert("Deepakkumar"); // error
+// check error or test js code we use console
+// to print some test/sample data on console
+
+console.log("Hello World");
+
+// variables ==> container which store data temp
+// we have 3 keyword to create a variables
+// var , let , const
+
+// keyword variableName = value
+
+// var :: change data any time
+// var :: again and again create sample variable
+var sName = "deepakkumar";
+var sName = "suraj";
+
+// let :: change data any time
+// let :: you can't create sample variable again and again
+let rollNumber = 10;
+let rollNumber = 50;
+
+// const :: you can't change value once it is set
+const marks = 20.5;
+
+console.log(sName);
+console.log(rollNumber);
+console.log(marks);
